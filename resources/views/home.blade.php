d6f6a40c0503edbf652dd1481da8d218682cb623
Irmayani3312301088 <yirma9867@gmail.com>
2024-03-08 15:02:40 +0700

    Update Readme,dan penambahan file baru home.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Tas</title>
+    <!-- link to css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Box icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <header>
+        <!-- logo -->
+        <a href="#" class="logo"><i class='bx bxs-shopping-bag'></i>ModaBags</a>
+        <!-- Menu Icon -->
+        <i class='bx bx-menu'></i>
+        <ul class="navbar">
+            <li><a href="#Beranda">Beranda</a></li>
+            <li><a href="#tentang kami">Tentang Kami</a></li>
+            <li><a href="#produk">Produk</a></li>
+            <li><a href="#kontak">Kontak</a></li>
+        </ul>
+
+        <div class="header-icon">
+        <i class='bx bx-search' ></i>
+        <i class='bx bxs-cart-alt' ><span>0</span></i>
+        </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\ListProductSController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama) {
+//     return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang', [ListBarangController::class, 'tampilkan']);
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () { 
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+  
+});
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/Home', [HomeController::class, 'home']);
